3fdd743ca4af3206da099dad08d6073d9418e0bc
SwayamAdhikary16 <swayamadhikary@Mac.domain.name>
2025-02-01 20:35:19 +0530

    task divided

--- utils/youtube_api.py
+++ utils/youtube_api.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-YT_API_KEY = 'AIzaSyDJ-l2v6FPelZESqC5TUM_6ae5LCmkowtE'   #os.getenv('YT_API')
+YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
+YT_API_KEY = os.getenv('YT_API')
 
 def fetch_video_data_numbers(video_id):
     
+
 
 
 
